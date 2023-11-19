faf705d6ebf41a58887571008ffcaed808830b93
DannyWinters <serena.nwinters@gmail.com>
2023-11-18 19:53:50 -0500

    AI Nav

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,55 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Movement : MonoBehaviour
 {
-    public Transform[] waypoints;
-    Transform target, restart;
+    public List<Transform> waypoint = new List<Transform>();
+    //public Transform[] waypoints;
+    //Transform target, restart;
+    NavMeshAgent agent;
+    float distance;
 
     public int index = 0, speed = 2, maxIndex = -1;
     // Start is called before the first frame update
     void Start()
     {
-        //foreach (Transform t in waypoints){
-        //    maxIndex++;
-        //}
-        
+        agent = GetComponent<NavMeshAgent>();
+        maxIndex = waypoint.Count;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if(this.transform.position == target.position)
+        //if (this.transform.position == target.position)
         //{
         //    index++;
-        //    if(index > maxIndex)
+        //    if (index > maxIndex)
         //    {
         //        index = 0;
         //    }
 
-        //    //waypointTravel();
+        waypointTravel();
         //}
-        if(this.transform.position == this.target.position)
-        {
-            transform.position = restart.position;
-        }
-        transform.position = Vector3.MoveTowards(this.transform.position, this.target.position, Time.deltaTime * speed);
+        //if (this.transform.position == this.target.position)
+        //{
+        //    transform.position = restart.position;
+        //}
+        //transform.position = Vector3.MoveTowards(this.transform.position, this.target.position, Time.deltaTime * speed);
     }
 
     void waypointTravel()
     {
-        target = waypoints[index];
-        transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
+        //target = waypoints[index];
+        //transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
+
+        distance = Vector3.Distance(waypoint[index].position, transform.position);
+        if(distance <= 2)
+        {
+            index++;
+            if(index >= maxIndex)
+            {
+                index = 0;
+            }
+        }
+        agent.SetDestination(waypoint[index].position);
     }
 
-    void setInitial()
-    {
-        restart = this.position;
-    }
-    Transform getInitial()
-    {
-        restart = this.position;
-    }
+    //void setInitial()
+    //{
+    //    restart = this.position;
+    //}
+    //Transform getInitial()
+    //{
+    //    restart = this.position;
+    //}
 
 }
